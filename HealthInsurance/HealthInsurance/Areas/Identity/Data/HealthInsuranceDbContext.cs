f85f4fedcdbb0fdc1f1d9a8a4080d9f7f33859ca
quantmth1908038 <54584382+quantmth1908038@users.noreply.github.com>
2021-01-13 21:24:27 +0700

    Page_commit

---
+++ HealthInsurance/HealthInsurance/Areas/Identity/Data/HealthInsuranceDbContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HealthInsurance.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace HealthInsurance.Data
+{
+    public class HealthInsuranceDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public HealthInsuranceDbContext(DbContextOptions<HealthInsuranceDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ HealthInsurance/HealthInsurance/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using HealthInsurance.Areas.Identity.Data;
+using HealthInsurance.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(HealthInsurance.Areas.Identity.IdentityHostingStartup))]
+namespace HealthInsurance.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<HealthInsuranceDbContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("HealthInsuranceDbContextConnection")));
+
+                services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<HealthInsuranceDbContext>();
+            });
+        }
+    }
+}
